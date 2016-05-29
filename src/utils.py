4bfb2a9e0db2ac32174092e2c96fc17cbcd643d8
adolenc <andrej.dolenc@student.uni-lj.si>
2016-05-29 17:28:15 +0200

    Add function to save matrix to file

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,13 @@ def load_directory(directory):
 def flatten(lst):
     """ Flatten shallow list LST. """
     return [e for l2 in lst for e in l2]
+
+def write_matrix(X, filename, separator=' '):
+    """ Write matrix X to file, using separator between two entries. First line
+    will contain number of rows and number of columns, separated by space.
+    """
+    with open(filename, 'w') as f:
+        rows, cols = len(X), len(X[0])
+        f.write(str(rows) + ' ' + str(cols) + '\n')
+        for row in X:
+            f.write(separator.join(map(str, row)) + '\n')
