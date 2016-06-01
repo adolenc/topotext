af4bbeda10e1ad8da9b341c35b2cc8e1eaa1ace4
adolenc <andrej.dolenc@student.uni-lj.si>
2016-06-01 18:08:04 +0200

    Fix cech_cx wrapper

--- src/cech.py
+++ src/cech.py
@@ -4,13 +4,13 @@ import shutil
 import os
 import subprocess
 
-def make_tmp_dir(tmpdir='tmp'):
+def make_tmp_dir(tmpdir):
     """ Create temporary directory. """
     if not os.path.exists(tmpdir):
         os.makedirs(tmpdir)
     return tmpdir
 
-def rm_tmp_dir(tmpdir='tmp'):
+def rm_tmp_dir(tmpdir):
     """ Remove temporary directory. """
     shutil.rmtree(tmpdir)
 
@@ -18,20 +18,17 @@ def cech(X, tmpdir='tmp', cech_cx_binary='./cpp/cech_cx'):
     """ Wrapper around cpp/cech_cx.cc in python. Creates a tmpdir directory
     which it uses for storing communication between two processes.
     """
+    make_tmp_dir(tmpdir)
     tmp_X_file =  tmpdir + '/X.mat'
     tmp_cech_cx_file = tmpdir + '/cech.cx'
-    make_tmp_dir(tmpdir)
     write_matrix(X, tmp_X_file)
     with open(tmp_X_file, 'r') as fi:
         with open(tmp_cech_cx_file, 'w') as fo:
             fo.write(subprocess.check_output([cech_cx_binary], stdin=fi))
+    cx = read_cx(tmp_cech_cx_file)
     rm_tmp_dir(tmpdir)
-    return read_cx(tmp_cech_cx_file)
+    return cx
 
 if __name__ == "__main__":
-    from preprocessor import *
-    from prepreprocessor import *
-    pp = Preprocessor(Prepreprocessor, [], use_tfidf=3)
-    X, _ = pp.process(['../data/abstracts/', '../data/sports'])
-    for sx in cech(X, tmpdir='cech_tmp'):
+    for sx in cech([[1,2,3], [3,4,5], [6,7,8], [9,1,2]], tmpdir='cech_tmp'):
         print sx
