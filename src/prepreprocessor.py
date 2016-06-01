0e39ee6723b045146f3087022237e2f949c78f2d
Peter Us <peter.us.99@gmail.com>
2016-06-01 15:28:37 +0200

    Fixed prepreprocessors to return array of tokenized sentences.

--- src/prepreprocessor.py
+++ src/prepreprocessor.py
@@ -1,32 +1,37 @@
+from nltk.tokenize import sent_tokenize
 from nltk.tokenize import RegexpTokenizer
 
 
 def BasicPrepreprocessor(text):
-    '''Basic preprocessor removes non alpha-numeric symbols
-    and capitalization'''
+    """Basic preprocessor removes non alpha-numeric symbols
+    and capitalization"""
 
     return Prepreprocessor(text, False, False, False)
 
 def Prepreprocessor(text, remove_stopwords=True, stem=True, lemmatize=True):
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = [w.lower() for w in tokenizer.tokenize(text)]
+    """
+    Tokenize text.
+    :return: Array of sentences. Each sentence is an array of words.
+    """
 
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = [[w.lower() for w in tokenizer.tokenize(sentence)] for sentence in sent_tokenize(text)]
     if remove_stopwords:
         from nltk.corpus import stopwords
         stops = set(stopwords.words('english'))
-        tokens = [w for w in tokens if w not in stops]
+        tokens = [[w for w in sentence if w not in stops] for sentence in tokens]
 
     if stem:
         from nltk.stem.porter import PorterStemmer
         stemmer = PorterStemmer()
-        tokens = [stemmer.stem(w) for w in tokens]
+        tokens = [[stemmer.stem(w) for w in sentence] for sentence in tokens]
 
     if lemmatize:
         from nltk.stem.wordnet import WordNetLemmatizer
         lmtzr = WordNetLemmatizer()
-        tokens = [lmtzr.lemmatize(w) for w in tokens]
+        tokens = [[lmtzr.lemmatize(w) for w in sentence] for sentence in tokens]
 
-    return tokens
+    return [token for token in tokens if token]
 
 
 if __name__ == '__main__':
