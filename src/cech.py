c2ad8e485a944225bea79551f67debe214c0e044
adolenc <andrej.dolenc@student.uni-lj.si>
2016-06-02 14:46:22 +0200

    Communicate with cech_cx via pipes instead of tmp files

--- src/cech.py
+++ src/cech.py
@@ -1,34 +1,19 @@
 from dionysus import *
-from utils import read_cx,write_matrix
-import shutil
-import os
+from utils import _read_cx,_write_matrix
+import StringIO
 import subprocess
 
-def make_tmp_dir(tmpdir):
-    """ Create temporary directory. """
-    if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
-    return tmpdir
-
-def rm_tmp_dir(tmpdir):
-    """ Remove temporary directory. """
-    shutil.rmtree(tmpdir)
-
-def cech(X, tmpdir='tmp', cech_cx_binary='./cpp/cech_cx'):
+def cech(X, cech_cx_binary='./cpp/cech_cx'):
     """ Wrapper around cpp/cech_cx.cc in python. Creates a tmpdir directory
     which it uses for storing communication between two processes.
     """
-    make_tmp_dir(tmpdir)
-    tmp_X_file =  tmpdir + '/X.mat'
-    tmp_cech_cx_file = tmpdir + '/cech.cx'
-    write_matrix(X, tmp_X_file)
-    with open(tmp_X_file, 'r') as fi:
-        with open(tmp_cech_cx_file, 'w') as fo:
-            fo.write(subprocess.check_output([cech_cx_binary], stdin=fi))
-    cx = read_cx(tmp_cech_cx_file)
-    rm_tmp_dir(tmpdir)
-    return cx
+    X_str = StringIO.StringIO()
+    _write_matrix(X, X_str)
+    pr = subprocess.Popen([cech_cx_binary], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    cech_cx_out = pr.communicate(input=X_str.getvalue())[0].decode()
+    return _read_cx(StringIO.StringIO(cech_cx_out))
 
 if __name__ == "__main__":
-    for sx in cech([[1,2,3], [3,4,5], [6,7,8], [9,1,2]], tmpdir='cech_tmp'):
+    X = [[1,2,3], [3,4,5], [6,7,8], [9,1,2]]
+    for sx in cech(X):
         print sx
