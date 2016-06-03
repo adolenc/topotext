c95a1dc6cb67f95f7d823f19991cbf556af31895
RokIvansek <ivansek.rok@gmail.com>
2016-06-03 11:29:47 +0200

    added single linkage as benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -5,16 +5,28 @@ from features_funcs import word_lengths_funcs, sentence_lengths_funcs, ratio_mos
 from operator import ge
 from sklearn.cluster import SpectralClustering,KMeans
 from sklearn.metrics import adjusted_rand_score,mutual_info_score
+from sklearn.decomposition import PCA
+import scipy.spatial.distance as ssd
+from scipy.cluster.hierarchy import linkage, fcluster
+from dionysus import PairwiseDistances,ExplicitDistances
 
 if __name__ == "__main__":
     funcs = [word_lengths_funcs, sentence_lengths_funcs, ratio_most_n_common_words, ratio_length_of_words_texts,
             lambda text: ratio_length_of_words_texts(text, 8, ge)]
     pp = Preprocessor(Prepreprocessor, funcs, use_tfidf=20)
     X, y = pp.process(['../data/abstracts/', '../data/sports', '../data/reviews'])
+    pca = PCA(3)
+    X_ = pca.fit_transform(X)
+    print X_
     sc = SpectralClustering(3)
     km = KMeans(3)
-    sc_pred = sc.fit_predict(X)
-    km_pred = km.fit_predict(X)
+    sc_pred = sc.fit_predict(X_)
+    km_pred = km.fit_predict(X_)
+    distances = PairwiseDistances(X_.tolist())
+    distances = ExplicitDistances(distances)
+    singlel_pred = fcluster(linkage(ssd.squareform(distances.distances)), 3, criterion='maxclust')
+    print "single-linkage clustering prediction:", singlel_pred
+    print "single-linkage clustering score:", adjusted_rand_score(y, singlel_pred), mutual_info_score(y, singlel_pred)
     print "spectral clustering prediction:", sc_pred
     print "spectral clustering score:", adjusted_rand_score(y, sc_pred), mutual_info_score(y, sc_pred)
     print "kmeans clustering prediction", km_pred
